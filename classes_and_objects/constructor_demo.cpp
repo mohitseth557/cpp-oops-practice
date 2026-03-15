68c43f64cd47402345cc239ef80143ffd761f461
mohitseth <sethmohit2006@gmail.com>
2026-03-16 02:30:33 +0530

    Add Student class with constructor and display method

---
+++ classes_and_objects/constructor_demo.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class Student {
+public:
+    string name;
+    int age;
+
+    Student(string n, int a) {
+        name = n;
+        age = a;
+    }
+
+    void display() {
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+    }
+};
+
+int main() {
+    Student s1("Mohit", 19);
+    s1.display();
+
+    return 0;
+}
